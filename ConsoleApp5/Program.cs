fef2369ed9c83d9a484613b746dd1b05fba3b890
Андрей Кузнецов <89307449447an@gmail.com>
2022-09-06 00:18:39 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Мама";
+            if ((str.StartsWith("А")) ^ (str.EndsWith("а")))
+            Console.Write("Слово начинается или заканчивается на букву А");
+            else
+            Console.Write("Слово не начинаеться и не заканчиватся на букву А");
+
+        }
+    }
+}
